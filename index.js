aaa8808ee363f271eec777543d6267a6f657e9b6
mawuse-tech <akpekemawuse1@gmail.com>
2025-09-11 08:24:58 +0000

    added netlify link

--- index.js
+++ index.js
@@ -13,10 +13,28 @@ const PORT = process.env.PORT || 6000
 const app = express();
 app.use(express.json());
 
+const allowedOrigins = [
+    /^http:\/\/localhost:\d+$/,              // allow any localhost port
+    "https://esseauth.netlify.app/"  // deployed frontend
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true  //sending cookies
-}))
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (
+            allowedOrigins.some((allowed) =>
+                allowed instanceof RegExp ? allowed.test(origin) : allowed === origin
+            )
+        ) {
+            return callback(null, true);
+        } else {
+            return callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
+
 
 app.use(cookieParser())
 
